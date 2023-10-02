3e7f32bb2d106cc90e8a5bcafd9aae8b8ed838dc
Guillermo Aprile <61857975+Guishe@users.noreply.github.com>
2023-10-02 01:38:21 -0300

    Agregado de Sonido y de GameOver. Guillermo Aprile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Play1 from "./Escenas/Play1.js";
 import Menu from "./Escenas/Menu.js";
-
+import GameOver from "./Escenas/gameOver.js";
 
 let config = {
     type: Phaser.AUTO,
@@ -13,7 +13,7 @@ let config = {
             debug: false
         }
     },
-    scene: [Menu,Play1]
+    scene: [Menu, Play1, GameOver]
 };
 
 let game = new Phaser.Game(config);
